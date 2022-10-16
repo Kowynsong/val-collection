f6cf3d584769c372c280240bb133ec335bb9ef12
Sunghwu Song <kowynsong@gmail.com>
2022-10-15 22:42:28 -0400

    basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,15 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="text-2xl text-red-200">Hello World</div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon is rounder?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200" />
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200" />
+      </div>
+    </div>
   )
 }
 
